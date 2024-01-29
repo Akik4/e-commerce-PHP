b1d65e5faa1f46645ce6bca806b69a8589494971
Stefan Dupont <dupontstef80@gmail.com>
2024-01-29 21:09:15 +0100

    adding redirect

--- public/action/register.php
+++ public/action/register.php
@@ -11,13 +11,13 @@ if (isset($_POST['username'])) {
     if (isset($_POST['password'])) {
         $password = hash('sha256', $_POST['password']);
         $data->insert_user($_POST['username'], $password);
-       // header("Location: login.php");
+        header("Location: login.php");
         echo "user created";
     } else {
-        //header("Location: register.php");
+        header("Location: register.php");
         echo "missing password :(";
     }
 } else {
-    //header("Location: register.php");
+    header("Location: register.php");
     echo "Missing username :(";
 }
