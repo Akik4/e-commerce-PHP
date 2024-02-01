9964464a9cb4ad041ccbcc6d664572206281986a
RobynNebula <kaihcy.pl@gmail.com>
2024-02-01 12:14:54 +0100

    basket page

---
+++ public/basket.php
@@ -0,0 +1,39 @@
+<?php
+    include_once "./template/header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="list">
+        <div class="list-container">
+            <table>
+                <thead>
+                <tr>
+                    <th></th><th>Product name</th><th>Quantity</th><th>Price</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+</body>
+</html>
+
