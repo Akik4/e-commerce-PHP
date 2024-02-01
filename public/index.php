df448a14225564c58ff0f0eda480b037b8a743bc
Stefan Dupont <dupontstef80@gmail.com>
2024-02-01 18:45:40 +0100

    index css fix

--- public/index.php
+++ public/index.php
@@ -13,27 +13,21 @@ include_once "./template/header.php";
             foreach ($rows as $row){
                 ?>
                 <a href="details.php?id=<?= $row['id'] ?>" class="card-link">
-                <div class="card">
-                    <div class="visual">
-                        <img src="<?= $row['image_url']?>" height="100%" width="100%">
+                    <div class="card">
+                        <div class="visual">
+                            <img src="<?= $row['image_url']?>" height="100%" width="100%">
+                        </div>
+
+                        <div class="card-info">
+                            <h4><?= $row['name'] ?></h4>
+                            <p><?= $row['price'] ?>$</p>
+                            <p><?= $row['description'] ?></p>
+                            <a href="action/add-cart.php?id=<?= $row['id']?>">+ add to cart</a>
+                        </div>
                     </div>
-                    <div class="card-info">
-                        <h4><?= $row['name'] ?></h4>
-                        <p><?= $row['price'] ?> $</p>
-        <!--                <p>--><?php //= $row[''] ?><!--</p>-->
-                    
-                    </div>
-                </div>
-                <div class="card-info">
-                    <h4><?= $row['name'] ?></h4>
-                    <p><?= $row['price'] ?>$</p>
-    <!--                <p>--><?php //= $row[''] ?><!--</p>-->
-                    <p><?= $row['description'] ?></p>
-                    <a href="action/add-cart.php?id=<?= $row['id']?>">+ add to cart</a>
-                </div>
-            </div>
                 </a>
-            
+
+
                 <?php
             }
         ?>
